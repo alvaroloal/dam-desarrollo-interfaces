9a3b8a0bc20e8efb5603c45d5c3e29b04a4f7793
alvaroloal <alorentealman@gmail.com>
2024-10-16 14:22:11 +0200

    ejercicio tabla alumnos

--- UD2_Angular/tabla-estudiantes/src/app/app.module.ts
+++ UD2_Angular/tabla-estudiantes/src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppComponent } from './app.component';
 import { TablaAlumnosComponent } from './components/tabla-alumnos/tabla-alumnos.component';
 import { MaterialImportModule } from './material-import-module/material-import-module.module';
 import { RouterModule } from '@angular/router';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TablaAlumnosComponent
+    TablaAlumnosComponent,//tabla
+
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialImportModule,
     RouterModule.forRoot([]),
+    MatCheckboxModule,//checkbox
+    MatMenuModule,//menu
   ],
   providers: [],
   bootstrap: [AppComponent]
--- UD2_Angular/tabla-estudiantes/src/app/components/tabla-alumnos/tabla-alumnos.component.ts
+++ UD2_Angular/tabla-estudiantes/src/app/components/tabla-alumnos/tabla-alumnos.component.ts
@@ -7,15 +7,42 @@ import { Student } from '../../model/estudiante';
   styleUrls: ['./tabla-alumnos.component.css']
 })
 export class TablaAlumnosComponent {
-  displayedColumns: string[] = ['numAlumno', 'nombre', 'apellidos', 'nif', 'edad', 'cursoMatriculado'];
-  dataSource: Student[] = [
-    { numAlumno: 1, nombre: 'Carlos', apellidos: 'García Pérez', nif: '12345678A', edad: 20, cursoMatriculado: '3º ESO' },
-    { numAlumno: 2, nombre: 'Pepe', apellidos: 'López Márquez', nif: '23456789B', edad: 21, cursoMatriculado: '4º ESO' },
-    { numAlumno: 3, nombre: 'Antonio', apellidos: 'López Márquez', nif: '23456789B', edad: 21, cursoMatriculado: '4º ESO' },
-    { numAlumno: 4, nombre: 'José', apellidos: 'López Márquez', nif: '23456789B', edad: 21, cursoMatriculado: '4º ESO' },
-    { numAlumno: 5, nombre: 'Manuel', apellidos: 'López Márquez', nif: '23456789B', edad: 21, cursoMatriculado: '4º ESO' },
-    { numAlumno: 6, nombre: 'Ana', apellidos: 'López Márquez', nif: '23456789B', edad: 21, cursoMatriculado: '4º ESO' },
-    { numAlumno: 7, nombre: 'Álvaro', apellidos: 'López Márquez', nif: '23456789B', edad: 21, cursoMatriculado: '4º ESO' }
+  allColumns: { name: string, label: string }[] = [
+    { name: 'numAlumno', label: 'Nº de Alumno' },
+    { name: 'nombre', label: 'Nombre' },
+    { name: 'apellidos', label: 'Apellidos' },
+    { name: 'nif', label: 'NIF' },
+    { name: 'edad', label: 'Edad' },
+    { name: 'cursoMatriculado', label: 'Curso Matriculado' }
   ];
-}
 
+  columnVisibility: { [key: string]: boolean } = {
+    numAlumno: true,
+    nombre: true,
+    apellidos: true,
+    nif: true,
+    edad: true,
+    cursoMatriculado: true
+  };
+
+  dataSource: Student[] = [
+    { numAlumno: 1, nombre: 'Carlos', apellidos: 'García Pérez', nif: '12345678A', edad: 20, cursoMatriculado: '2º DAM' },
+    { numAlumno: 2, nombre: 'Pepe', apellidos: 'Ruiz López', nif: '23456789B', edad: 21, cursoMatriculado: '2º DAM' },
+    { numAlumno: 3, nombre: 'Antonio', apellidos: 'Ramirez Núñez', nif: '23456789B', edad: 22, cursoMatriculado: '2º DAM' },
+    { numAlumno: 4, nombre: 'José', apellidos: 'López Márquez', nif: '23456789B', edad: 22, cursoMatriculado: '2º DAM' },
+    { numAlumno: 5, nombre: 'Manuel', apellidos: 'Alcaraz Guitierrez', nif: '23456789B', edad: 21, cursoMatriculado: '1º DAM' },
+    { numAlumno: 6, nombre: 'Ana', apellidos: 'Ruiz Alonso', nif: '23456789B', edad: 21, cursoMatriculado: '1º DAM' },
+    { numAlumno: 7, nombre: 'Álvaro', apellidos: 'López Márquez', nif: '23456789B', edad: 21, cursoMatriculado: '1º DAM' },
+    { numAlumno: 8, nombre: 'Eva', apellidos: 'Ruiz Alonso', nif: '23456789B', edad: 22, cursoMatriculado: '2º DAM' }
+  ];
+
+  get displayedColumns(): string[] {
+    return this.allColumns
+      .filter(col => this.columnVisibility[col.name])
+      .map(col => col.name);
+  }
+
+  toggleColumn(column: string) {
+    this.columnVisibility[column] = !this.columnVisibility[column];
+  }
+}
